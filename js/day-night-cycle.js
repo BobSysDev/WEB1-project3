37be155c4402e0811d0d775d74bce43d4cfd76c9
Electimore <61228588+Electimore@users.noreply.github.com>
2023-11-09 21:09:38 +0100

    ♻️💡Refactored the code and added some commnets

--- js/day-night-cycle.js
+++ js/day-night-cycle.js
@@ -1,17 +1,23 @@
+//This part of code is done by Aleksander Gwóźdź
+
+//declaring variables - jQuery objects and values
 var backgroud = $("body");
 var sun = $("#sun");
 var moon = $("#moon");
 var screenwidth = $(window).width();
 var screenheight = $(window).height();
 
+//hiding the moon in the beginning of the animation
 moon.css({
     visibility: "hidden"
 });
 
+//function to change background color to a custom r, g and b values
 function changeBgColor(r, g, b){
     backgroud.css("background-color", "rgb(" + r + "," + g + "," + b + ")");
 }
 
+//declaring variables - logic
 var rs = 255;
 var gs = 255;
 var bs = 255;
@@ -22,14 +28,17 @@ var be = 75;
 
 var day = true;
 
-var transitionDuration = 10000;
+const transitionDuration = 10000; // < this sets how long a day lasts (and night too), in miliseconds
 
+
+// when the document fully loads, the animation starts
 $(document).ready(function(){
     changeBgColor(rs, gs, bs);
     transitionBgColor();
     movementStart();
 });
 
+//function to change background color from one set of r, g and b values to another
 function transitionBgColor(){
     $({r: rs, g: gs, b: bs}).animate({r: re, g: ge, b: be}, {
         duration: transitionDuration,
@@ -38,27 +47,20 @@ function transitionBgColor(){
         },
     });
 
-    setTimeout(function(){
-        let temp = rs;
-        rs = re;
-        re = temp;
-        temp = gs;
-        gs = ge;
-        ge = temp;
-        temp = bs;
-        bs = be;
-        be = temp;
+    setTimeout(function(){ //swapping the values so the animation can go on forever, day to night and night to day
+        [rs, gs, bs, re, ge, be] = [re, ge, be, rs, gs, bs]; //i hate this line of code but it allowed me to compress 9 lines into a single one so i guess it's fine (+ it works :>)
 
         transitionBgColor();
     }, transitionDuration);
 }
 
+//function to jump-start the animation from half-way through the day so the sun and moon are in sync with the background color
 function movementStart(){
-    var startH = 0
-    var endH = screenheight/2 - 240/2;
+    let startH = 0
+    let endH = screenheight/2 - 240/2;
 
-    var startW = screenwidth/2 - 240/2;
-    var endW = screenwidth + 240;
+    let startW = screenwidth/2 - 240/2;
+    let endW = screenwidth + 240;
 
     sun.css({
         top: startH + "px",
@@ -79,22 +81,23 @@ function movementStart(){
     
     });
 
-    setTimeout(function(){
+    setTimeout(function(){ //starting to move the sun and the moon in a loop
         day = false;
-        moveSun();
+        moveSunOrMoon();
     }, transitionDuration/2);
 }
 
-function moveSun(){
-    var startH = screenheight/2 - 240/2;
-    var endH = screenheight/2 - 240/2;
+//function to move the sun and the moon from one side of the screen to the other
+function moveSunOrMoon(){
+    let startH = screenheight/2 - 240/2;
+    let endH = screenheight/2 - 240/2;
 
-    var startW = -240;
-    var endW = screenwidth + 240;
+    let startW = -240;
+    let endW = screenwidth + 240;
 
-    var objectToMove;
+    let objectToMove;
 
-
+    //checking if it's day or night and moving the sun or the moon accordingly + hiding the other one
     if(day){
         sun.css({
             visibility: "visible",
@@ -123,7 +126,7 @@ function moveSun(){
         duration: transitionDuration,
         easing: "linear",
         step: function(){
-            let heightQuardatic = (((screenwidth/2 - this.w)/60)*((screenwidth/2 - this.w)/60));
+            let heightQuardatic = (((screenwidth/2 - this.w)/60)*((screenwidth/2 - this.w)/60)); //this is a quadratic function that makes the sun and the moon move in a parabolic arc instead of a straight line
 
             objectToMove.css({
                 top: Math.floor(heightQuardatic) + "px",
@@ -133,7 +136,7 @@ function moveSun(){
     
     });
 
-    setTimeout(function(){
+    setTimeout(function(){ //swapping the day/night variable and calling the function again
         day = !day;
         moveSun();
     }, transitionDuration);
--- js/watering-can.js
+++ js/watering-can.js
@@ -1,17 +1,21 @@
 //This part of code is done by Aleksander Gwóźdź
 
+//declaring variables - jQuery objects and values
 var wateringCan = $("#wateringcan");
-var tilted = false;
 var waterdrops = $(".waterdrop");
 var screenheight = $(window).height();
 
+//declaring variables - logic
+var tilted = false;
+
+//hiding waterdrops
 waterdrops.each(function(){
     $(this).css({
         visibility: 'hidden'
     });
 });
 
-
+//rotating watering can on click
 wateringCan.click(function () {
     if(!tilted){
         $({deg: 0}).animate({deg: -30}, {
@@ -23,13 +27,13 @@ wateringCan.click(function () {
             }
         })
         tilted = true;
-        setTimeout(function(){
+        setTimeout(function(){ //starting pouring after 350ms
             waterdrops.each(function(){
                 pour($(this));
             });
         }, 350);
     }
-    else{
+    else{ //rotating back if the can is tilted
         $({deg: -30}).animate({deg: 0}, {
             duration: 500,
             step: function(now) {
@@ -42,7 +46,8 @@ wateringCan.click(function () {
     }
 });
 
-function pour(object){
+//pouring function - called separately for each waterdrop
+function pour(object){ 
     var startV = wateringCan.position().top + Math.random() * 20 + 50;
     var startH = wateringCan.position().left + Math.random() * 20 - 10;
     var speed = Math.random() * 1200 + 800;
@@ -54,7 +59,7 @@ function pour(object){
         visibility: 'visible'
     });
 
-    $({pos: startV}).animate({pos: endV}, {
+    $({pos: startV}).animate({pos: endV}, { //animating waterdrops
         duration: speed,
         step: function(now) {
             object.css({
@@ -64,7 +69,7 @@ function pour(object){
         
     });
     
-    setTimeout(function(){
+    setTimeout(function(){ //recursively calling pour() after random time (same as it takes the waterdrops to fall to the bottom of the screen)
         object.css({
             visibility: 'hidden'
         });
