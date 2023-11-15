463cf93abfb6c3ea96d7671557e21cf6d6b5314b
Marius Gafton <mariusgafton2005@gmail.com>
2023-11-15 23:55:56 +0100

    RAIN!!!

--- js/rain.js
+++ js/rain.js
@@ -1,24 +1,23 @@
 $(document).ready(function() {
-    function createRaindrop() {
-      const raindrop = $('<div class="rain"></div>').css({
-        left: Math.random() * $(window).width(),
-        top: -100,
-        opacity: Math.random() + 0.5,
-        transform: 'rotate(' + Math.random() * 360 + 'deg)'
-      });
-      $('body').append(raindrop);
-      
-      raindrop.animate({
-        top: $(window).height() + 100,
-        left: '+=20'
-      }, Math.random() * 2000 + 1500, 'linear', function() {
-        $(this).remove();
-        createRaindrop();
-      });
-    }
-  
-    // Create multiple raindrops
-    for (let i = 0; i < 30; i++) {
+  function createRaindrop() {
+    const raindrop = $('<div class="raindrop"></div>').css({
+      left: Math.random() * ($(window).width() - 20), // Adjust the subtraction to fit the raindrop width
+      top: -100,
+      opacity: Math.random() + 0.5,
+    });
+    $('body').append(raindrop);
+    
+    raindrop.animate({
+      top: $(window).height() + 100,
+      left: '+=' + Math.random() * 100, // Adjust the random value for horizontal movement
+    }, Math.random() * 2000 + 1500, 'linear', function() {
+      $(this).remove();
       createRaindrop();
-    }
-  });
+    });
+  }
+
+  // Create multiple raindrops
+  for (let i = 0; i < 30; i++) {
+    createRaindrop();
+  }
+});
