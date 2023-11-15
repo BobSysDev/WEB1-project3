e5d04700e979469f8f6ff7031f7f8f09cc56928b
Marius Gafton <mariusgafton2005@gmail.com>
2023-11-16 00:21:33 +0100

    rain

--- js/rain.js
+++ js/rain.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
   function createRaindrop() {
     const raindrop = $('<div class="raindrop"></div>').css({
-      left: Math.random() * ($(window).width() - 20), // Adjust the subtraction to fit the raindrop width
-      top: -100,
+      left: Math.random() * ($(window).width() - 20),
+      top: -3000, // Adjusted initial top position to start above the viewport
       opacity: Math.random() + 0.5,
     });
     $('body').append(raindrop);
     
     raindrop.animate({
       top: $(window).height() + 100,
-      left: '+=' + Math.random() * 100, // Adjust the random value for horizontal movement
+      left: '+=' + Math.random() * 100,
     }, Math.random() * 2000 + 1500, 'linear', function() {
       $(this).remove();
-      createRaindrop();
     });
+    
+    setTimeout(createRaindrop, Math.random() * 1000 + 500);
   }
 
-  // Create multiple raindrops
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 15; i++) {
     createRaindrop();
   }
 });
