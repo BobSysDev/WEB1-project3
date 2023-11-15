555e84693f14efdd8742ee621573248c2f14257c
Marius Gafton <mariusgafton2005@gmail.com>
2023-11-15 23:10:05 +0100

    rain stuff

--- js/rain.js
+++ js/rain.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
-    function createRain() {
-        $('.rain').append('<div class="raindrop"></div>');
+    function createRaindrop() {
+        // Create a raindrop element
+        var raindrop = $('<div class="raindrop"></div>');
+
+        // Calculate random position within the width of the garden
+        var posX = Math.random() * $('.garden').width();
+        
+        // Set initial position for the raindrop
+        raindrop.css({
+            left: posX,
+            top: '-50px' /* Start position above the garden */
+        });
+
+        // Append the raindrop to the rain container
+        $('.rain').append(raindrop);
+
+        // Animate the raindrop falling
+        raindrop.animate({
+            top: '100%' /* End position at the bottom of the garden */
+        }, 3000, 'linear', function() {
+            // Remove the raindrop after animation completes
+            $(this).remove();
+        });
     }
 
-    setInterval(createRain, 200); // Adjust timing for raindrop creation
-
-    // Optional: Remove raindrops after reaching the bottom to avoid performance issues
-    $('.rain').on('animationiteration', '.raindrop', function() {
-        $(this).remove();
-    });
+    // Call createRaindrop function at intervals for continuous rain
+    setInterval(createRaindrop, 200); // Adjust timing for raindrop creation
 });
